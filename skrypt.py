40cdccd7f229c9e3dd63d88ff854573482ddcfa3
Maciek <maciek.wis12@gmail.com>
2023-04-24 12:05:34 +0200

    Utworzenie klasy Transformacje & zdefiniowanie def __init__

--- skrypt.py
+++ skrypt.py
@@ -5,3 +5,28 @@ Created on Wed Apr 19 17:15:28 2023
 @author: Lenovo
 """
 
+from math import sqrt, sin, cos, atan, atan2, degrees
+
+
+class Transformacje:
+    def __init__(self, model: str = 'wgs84'):
+        """
+        Parametry elipsoid:
+            a - duża półoś elipsoidy - promień równikowy
+            b - mała półoś elipsoidy - promień południkowy
+            flat - spłaszczenie
+            e2 - mimośród^2
+        """
+        if model == "wgs84":
+            self.a = 6378137.0
+            self.b = 6356752.31424518
+        elif model == "grs80":
+            self.a = 6378137.0
+            self.b = 6356752.31414036
+        else:
+            raise NotImplementedError(f"{model} model not implemented")
+        self.flat = (self.a - self.b) / self.a
+        self.e = sqrt(2 * self.flat - self.flat ** 2) # eccentricity
+        self.e2 = (2 * self.flat - self.flat ** 2) # eccentricity**2
+
+    
